81dc5cc4a71c2c2cca094d648a686f2847566fb3
andyye <63383967+predict-woo@users.noreply.github.com>
2024-01-10 21:21:27 +0900

    Create kaist.is-an.app.js

---
+++ domains/kaist.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'A website for my server at KAIST',
+  domain: 'is-an.app',
+  subdomain: 'kaist',
+  owner: {
+    email: 'andyye.jongcye@gmail.com',
+  },
+  record: {
+    A: ['130.162.135.186'],
+  },
+  proxy: false
+});
